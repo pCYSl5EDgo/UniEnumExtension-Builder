eb2d8bf325ec45c0264143ca73135ae9b59a26e1
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-29 22:17:11 +0900

    [init]

---
+++ Assets/Scripts/EnumAssembly/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Flags]
+public enum TestEnum2 : long
+{
+    z = 0,
+    a = 1,
+    b = 2,
+    c = 4,
+    d = 8,
+    e = 0x10,
+    f = 0x20,
+    g = 0x40,
+    h = 0x80,
+}
