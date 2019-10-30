fbe4a6990f8808b36ed7e37ef58bfbe9cdcd8801
pcysl5edgo <pcysl5edgo@gmail.com>
2019-10-30 14:28:58 +0900

    [update]Publish

--- Assets/Editor/CreateUnityPackage.cs
+++ Assets/Editor/CreateUnityPackage.cs
@@ -1,14 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using Editor;
 using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using PackageInfo = UnityEditor.PackageInfo;
 
 public static class CreateUnityPackage
 {
+    public static void PrintVersion()
+    {
+        File.WriteAllText(Application.dataPath + "/../../version.txt", PlayerSettings.bundleVersion);
+    }
+    
+    public static void UpdatePackageVersion()
+    {
+        var path = Application.dataPath + "/Plugins/UEE/package.json";
+        var packageJson = JsonUtility.FromJson<PackageJson>(File.ReadAllText(path));
+        packageJson.version = PlayerSettings.bundleVersion;
+        File.WriteAllText(path, packageJson.ToString());
+    }
+
+    public static void IncrementBuildVersion()
+    {
+        var bundleVersion = PlayerSettings.bundleVersion.Split('.');
+        ref var buildVersion = ref bundleVersion[bundleVersion.Length - 1];
+        if (!int.TryParse(buildVersion, out var buildNumber))
+        {
+            Console.Error.WriteLine(buildVersion + " is not valid build number : " + bundleVersion);
+        }
+        buildVersion = (buildNumber + 1).ToString();
+        bundleVersion.ToBundleVersion();
+    }
+
+    private static void ToBundleVersion(this IReadOnlyList<string> bundleVersion)
+    {
+        PlayerSettings.bundleVersion = bundleVersion.Skip(1).Aggregate(new StringBuilder(bundleVersion[0]), (builder, str) => builder.Append('.').Append(str), builder => builder.ToString());
+    }
+
+    public static void IncrementMinorVersionAndResetBuildVersion()
+    {
+        var bundleVersion = PlayerSettings.bundleVersion.Split('.');
+        ref var buildVersion = ref bundleVersion[bundleVersion.Length - 1];
+        ref var minorVersion = ref bundleVersion[bundleVersion.Length - 2];
+        if (!int.TryParse(minorVersion, out var minorNumber))
+        {
+            Console.Error.WriteLine(minorNumber + " is not valid " + nameof(minorNumber) + " : " + bundleVersion);
+        }
+        buildVersion = "0";
+        minorVersion = (minorNumber + 1).ToString();
+        bundleVersion.ToBundleVersion();
+    }
+
     public static void CreateBOOTH()
     {
-        // configure
         const string root = "Plugins/UEE";
         var exportPath = Path.Combine(Application.dataPath + "/", "../../artifact_booth/UniEnumExtension.unitypackage");
 
@@ -21,7 +69,7 @@ public static class CreateUnityPackage
                 return extension == ".cs" || extension == ".asset" || extension == ".bytes" || extension == ".asmdef";
             })
             .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
-            .Concat(new []
+            .Concat(new[]
             {
                 dataPath + "LICENSE-Commercial",
                 dataPath + "README",
@@ -41,7 +89,6 @@ public static class CreateUnityPackage
 
     public static void CreateGplV3()
     {
-        // configure
         const string root = "Plugins/UEE";
         var exportPath = Path.Combine(Application.dataPath + "/", "../../artifact_gplv3/UniEnumExtension.unitypackage");
 
@@ -54,7 +101,7 @@ public static class CreateUnityPackage
                 return extension == ".cs" || extension == ".asset" || extension == ".bytes" || extension == ".asmdef";
             })
             .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
-            .Concat(new []
+            .Concat(new[]
             {
                 dataPath + "LICENSE-GPLv3",
                 dataPath + "package.json",
---
+++ Assets/Editor/PackageJson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+namespace Editor
+{
+    [Serializable]
+    public sealed class PackageJson
+    {
+        public string name;
+        public string displayName;
+        public string version;
+        public string unity;
+        public string description;
+        public string category;
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder(JsonUtility.ToJson(this));
+            while (true)
+            {
+                var lastChar = builder[builder.Length - 1];
+                builder.Length--;
+                if (lastChar == '}')
+                {
+                    break;
+                }
+            }
+            return builder.Append(",\"dependencies\":{\"nuget.mono-cecil\":\"0.1.6-preview\"}}").ToString();
+        }
+    }
+    
+}
